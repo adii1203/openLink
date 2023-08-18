8f96a7f914bc39ce21ac4180ca0774d212ea2035
adii1203 <aditya32ft@gmail.com>
2023-08-18 13:37:34 +0530

    ui-fix

--- src/coponents/dashboard/Loading.jsx
+++ src/coponents/dashboard/Loading.jsx
@@ -1,28 +1,30 @@
 const Loading = () => {
   return (
     <>
-      <div className="animate-pulse mb-4">
-        <div className="bg-gray-400/10 py-2 px-4 rounded-xl lg:w-1/2 mx-auto">
-          <div className=" h-6 w-1/2 bg-black/20 rounded-full"></div>
-          <div className=" mt-4 bg-black/20 w-[60%] rounded-full h-4"></div>
+      <div className=" z-50">
+        <div className="animate-pulse mb-4">
+          <div className="bg-gray-400/10 py-2 px-4 rounded-xl lg:w-1/2 mx-auto">
+            <div className=" h-6 w-1/2 bg-black/20 rounded-full"></div>
+            <div className=" mt-4 bg-black/20 w-[60%] rounded-full h-4"></div>
+          </div>
         </div>
-      </div>
-      <div className="animate-pulse mb-4">
-        <div className="bg-gray-400/10 py-2 px-4 rounded-xl lg:w-1/2 mx-auto">
-          <div className=" h-6 w-1/2 bg-black/20 rounded-full"></div>
-          <div className=" mt-4 bg-black/20 w-[60%] rounded-full h-4"></div>
+        <div className="animate-pulse mb-4">
+          <div className="bg-gray-400/10 py-2 px-4 rounded-xl lg:w-1/2 mx-auto">
+            <div className=" h-6 w-1/2 bg-black/20 rounded-full"></div>
+            <div className=" mt-4 bg-black/20 w-[60%] rounded-full h-4"></div>
+          </div>
         </div>
-      </div>
-      <div className="animate-pulse mb-4">
-        <div className="bg-gray-400/10 py-2 px-4 rounded-xl lg:w-1/2 mx-auto">
-          <div className=" h-6 w-1/2 bg-black/20 rounded-full"></div>
-          <div className=" mt-4 bg-black/20 w-[60%] rounded-full h-4"></div>
+        <div className="animate-pulse mb-4">
+          <div className="bg-gray-400/10 py-2 px-4 rounded-xl lg:w-1/2 mx-auto">
+            <div className=" h-6 w-1/2 bg-black/20 rounded-full"></div>
+            <div className=" mt-4 bg-black/20 w-[60%] rounded-full h-4"></div>
+          </div>
         </div>
-      </div>
-      <div className="animate-pulse mb-4">
-        <div className="bg-gray-400/10 py-2 px-4 rounded-xl lg:w-1/2 mx-auto">
-          <div className=" h-6 w-1/2 bg-black/20 rounded-full"></div>
-          <div className=" mt-4 bg-black/20 w-[60%] rounded-full h-4"></div>
+        <div className="animate-pulse mb-4">
+          <div className="bg-gray-400/10 py-2 px-4 rounded-xl lg:w-1/2 mx-auto">
+            <div className=" h-6 w-1/2 bg-black/20 rounded-full"></div>
+            <div className=" mt-4 bg-black/20 w-[60%] rounded-full h-4"></div>
+          </div>
         </div>
       </div>
     </>
